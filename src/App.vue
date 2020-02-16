198cf4d6616d1cc7782572fe0f7db086a298e7e0
wolf-tungsten <canis.tungsten@foxmail.com>
2020-02-16 23:54:51 +0800

    首次提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>你能看到这些文字吗？</h1>
+    <p>我们无意中发现一个特殊的网站 favicon 设置会导致微信 7.0.10 WebView 全面崩溃</p>
+    <p>而出现崩溃的机型集中在华为特定型号</p>
+    <p>这些手机不但无法访问本网站，甚至在访问本网站之后无法打开任何其他网站</p>
+    <p>这个网站只是一个 vue-cli 创建的模板项目，除了一些文字展示没有其他任何逻辑</p>
+    <p>我们把项目源码放置在：https://github.com/Wolf-Tungsten/wawei-crash</p>
+    <p>希望有人能够解释其中的原理</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
